7ec404a37ded3f21021d04d9cf7725a141a3b087
JJ-White <juriaanmoonen@hotmail.com>
2024-02-12 12:50:04 +0100

    Add pwm steps to avoid resonances

--- radiator_fan_control.ino
+++ radiator_fan_control.ino
@@ -2,7 +2,7 @@
 
 #define ntc_thresh 500
 #define ntc_hyst 10
-#define ntc_max 260 // 300~50c, 260~55c 
+#define ntc_max 280 // 300~50c, 260~55c 
 #define pwm_min 30
 #define sensor_avg_weight 10
 
@@ -11,7 +11,9 @@
 #define pwm_pin 3
 #define turbo_pin 2
 
-int pwm_max, pwm_val;
+// PWM steps chosen manually to avoid resonances
+int pwm_steps[] = {pwm_min, 60, 90, 120, 150};
+int s;
 int ntc = ntc_thresh;
 
 void setup() {
@@ -29,26 +31,29 @@ void setup() {
 }
 
 void loop() {
+//  analogWrite(pwm_pin, map(analogRead(pot_pin), 1023, 0, pwm_min, 255));
+//  Serial.print(" pwm: ");
+//  Serial.println(map(analogRead(pot_pin), 1023, 0, pwm_min, 255));
+//  delay(500);
+//  return;
+
   ntc = ((ntc * (sensor_avg_weight - 1)) + analogRead(ntc_pin)) / sensor_avg_weight;
-  pwm_max = map(analogRead(pot_pin), 1023, 0, pwm_min, 255);
-  pwm_val = map(ntc, ntc_thresh, ntc_max, pwm_min, pwm_max);
-  if (pwm_val < pwm_min)
-    pwm_val = pwm_min;
-  else if (pwm_val > pwm_max)
-    pwm_val = pwm_max;
+  s = map(ntc, ntc_thresh, ntc_max, 0, 4);
+  if (s < 0) s = 0;
+  else if (s > 4) s = 4;
 
 #ifdef debug
   Serial.print("ntc: ");
   Serial.print(ntc);
   Serial.print(" pwm_max: ");
-  Serial.print(pwm_max);
-  Serial.print(" pwm_val: ");
-  Serial.println(pwm_val);
+  Serial.print(pwm_steps[4]);
+  Serial.print(" pwm: ");
+  Serial.println(pwm_steps[s]);
 #endif
 
   if (ntc < ntc_thresh - ntc_hyst) {
     if (!digitalRead(turbo_pin)) analogWrite(pwm_pin, 255);
-    else analogWrite(pwm_pin, pwm_val);
+    else analogWrite(pwm_pin, pwm_steps[s]);
   }
   else if (ntc > ntc_thresh + ntc_hyst) {
     analogWrite(pwm_pin, 0);
